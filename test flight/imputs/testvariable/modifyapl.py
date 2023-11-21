952d63b29a64bcfe73a2c69fb9f293a6c47e3f5d
irene146 <irene.monrealcampos@ncas.ac.uk>
2023-11-21 17:25:29 +0000

    organisation and planning

--- test flight/imputs/testvariable/modifyapl.py
+++ test flight/imputs/testvariable/modifyapl.py
@@ -3,5 +3,5 @@ from modelconfig import ModelConfig
 mc = ModelConfig()
 mc.read('davetest.ini')
 mc['ADMS_SOURCE_DETAILS']['SrcPolEmissionRate'] = '1.7e+0'
-mc.write('davetest3.apl')
+mc.write('davetest1.apl')
 
